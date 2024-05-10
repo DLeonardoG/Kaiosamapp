f6b46d0799e1d67128484dbacbdc22613ec203da
DLeonardoG <darkleonardog@gmail.com>
2024-05-10 11:25:28 -0500

    ya puedo añadir y eliminar como admin

--- kayosaimapp/modules/crud_admin.py
+++ kayosaimapp/modules/crud_admin.py
@@ -11,10 +11,10 @@ def registrar_user(datos):
         
     except Exception as e:
         reportar_error_a_txt(e)
-        usuarios["edad"] = 0
+        usuarios["edad"] = 18
+    usuarios["documento"]=input("Ingrese el documento: ")
     usuarios["nombre"]=input("Ingrese el nombre: ")
     usuarios["apellido"]=input("Ingrese el apellido: ")
-    usuarios["documento"]=input("Ingrese el documento: ")
 
 
     
@@ -22,4 +22,25 @@ def registrar_user(datos):
     print("usuarios registrado con éxito!")
     return datos
 
-registrar_user(datos)
+#registrar_user(datos)
+
+
+def eliminar_user(datos):
+    datos = dict(datos)
+    documento =input("Ingrese el documento del usuario: ")
+    for i in range(len(datos["usuarios"])):
+        if datos["usuarios"][i]["documento"] == documento:
+            datos["usuarios"].pop(i)
+            print("usuario eliminado!")
+    return datos
+
+#eliminar_user(datos)
+
+def actualizar_user(datos):
+    datos = dict(datos)
+    documento =input("Ingrese el documento del participante: ")
+    for i in range(len(datos["usuarios"])):
+        if datos["usuarios"][i]["documento"] == documento:
+            print ("¿Desea actualizar el status?")
+
+guardar_datos(datos, RUTA_BASE_DE_DATOS_USERS)
