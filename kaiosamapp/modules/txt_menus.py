a8f0d9a11f5799e9f5f2e8cf9bf050997ec931c9
DLeonardoG <166556041+DLeonardoG@users.noreply.github.com>
2024-05-11 16:39:11 +0000

    funcion de txt con variables con nombre del menu txt

--- kaiosamapp/modules/txt_menus.py
+++ kaiosamapp/modules/txt_menus.py
@@ -1,19 +1,17 @@
+def mostrar_txt(ruta_archivo):
+    with open(ruta_archivo, 'r') as archivo:
+        contenido = archivo.read()
+        print(contenido)
 
-def menu_txt(archivo):
-    with open(archivo, 'r') as archive:
-        menu = archive.read()
-        print(menu)
-
-principal = "/workspaces/proyecto/kaiosamapp/txt/menus_plantillas/principal.txt"
-menu_txt(principal)
+principal = "kaiosamapp/txt/menus_plantillas/principal.txt"
+mostrar_txt(principal)
 
 _1 = "kaiosamapp/txt/menus_plantillas/1.txt"
-
 _1_1 = "kaiosamapp/txt/menus_plantillas/1-1.txt"
 _1_2 = "kaiosamapp/txt/menus_plantillas/1-2.txt"
-_1_3 = ""
-_1_4 = ""
-_1_4_2 = ""
-_1_4_3 = ""
-_1_4_2_2 = ""
-_1_4_3_2 = ""
+_1_3 = "kaiosamapp/txt/menus_plantillas/1-3.txt"
+_1_4 = "kaiosamapp/txt/menus_plantillas/1-4.txt"
+_1_4_2 = "kaiosamapp/txt/menus_plantillas/1-4-2.txt"
+_1_4_3 = "kaiosamapp/txt/menus_plantillas/1-4-3.txt"
+_1_4_2_4 = "kaiosamapp/txt/menus_plantillas/1-4-2-4.txt"
+_1_4_3_4 = "kaiosamapp/txt/menus_plantillas/1-4-3-4.txt"
