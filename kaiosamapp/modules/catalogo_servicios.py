9bc93f97ca28e2c9aef3b85ebbe4600f04e1c700
DLeonardoG <166556041+DLeonardoG@users.noreply.github.com>
2024-05-13 01:34:50 +0000

    crear un servicio esta listo con todas validaciones y guarda adecuadamente

--- kaiosamapp/modules/catalogo_servicios.py
+++ kaiosamapp/modules/catalogo_servicios.py
@@ -1,7 +1,8 @@
 from modules.datos_catalogo import *
 from modules.funciones_secundarias import reportar_error_a_txt,opcion_no_valida
 from modules.funciones_secundarias import clear_screen,very
-from modules.catalogo_servicios_funciones import id_valido
+from modules.catalogo_servicios_funciones import id_valido,tipo_servicios,int_precio
+from modules.catalogo_servicios_funciones import int_nuevo,int_regular,int_leal
 
 def registrar_servicio(datos):
     datos = dict(datos)
@@ -16,16 +17,16 @@ def registrar_servicio(datos):
     except Exception as e:
         reportar_error_a_txt(e)
         catalogo_servicios["id"] = ""
-    catalogo_servicios["tipo_servicios"]=input("Ingrese el tipo de servicio: ")
+    tipo_servicios(catalogo_servicios)
     catalogo_servicios["referencia"]=input("Ingrese la referencia: ")
     catalogo_servicios["plan"]=input("Ingrese el plan: ")
-    catalogo_servicios["precio"]=input("Ingrese el precio: ")
+    int_precio(catalogo_servicios)
     catalogo_servicios["duracion"]=input("Ingrese la duracion: ")
     catalogo_servicios["descripcion"]=input("Ingrese la descripcion: ")
-    catalogo_servicios["descuento"]={"nuevo":"","regular":"","leal":""}
-    catalogo_servicios["descuento"]["nuevo"]=input("Ingrese el descuento para usuarios nuevos: ")
-    catalogo_servicios["descuento"]["regular"]=input("Ingrese el descuento para usuarios regulares: ")
-    catalogo_servicios["descuento"]["leal"]=input("Ingrese el descuento para usuarios leales: ")
+    catalogo_servicios["descuento"]={"nuevo": 0,"regular":0,"leal":0}
+    int_nuevo(catalogo_servicios)
+    int_regular(catalogo_servicios)
+    int_leal(catalogo_servicios)
     
     datos["catalogo_servicios"].append(catalogo_servicios)
     print( catalogo_servicios["referencia"],catalogo_servicios["plan"],"registrado con éxito!")
--- kaiosamapp/modules/catalogo_servicios_funciones.py
+++ kaiosamapp/modules/catalogo_servicios_funciones.py
@@ -16,9 +16,82 @@ def id_valido(catalogo_servicios):
     while True:
         int_id(catalogo_servicios)
         cad = str(catalogo_servicios["id"])
-        if len(cad) == 5: return catalogo_servicios["id"]
+        if len(cad) == 5: return str(catalogo_servicios["id"])
         else:
             dm = "Error al registrar id"
             reportar_error_a_txt(dm)
             print("El numero de id debe tener 5 digitos")
             catalogo_servicios["id"] = ""
+            
+def tipo_servicios(catalogo_servicios):    
+    while True:
+        op = input("Ingrese el tipo_servicios: \n    1. Fibra optica Namekusei\n    2. Pospago Roshi\n    3. Prepago Krillin\n    4. Combo Dragon\n>>    ")
+        if op == "1": 
+            catalogo_servicios["tipo_servicios"] = "fibra_optica_namekusei"                    
+            break
+        elif op == "2":
+            catalogo_servicios["tipo_servicios"] = "pospago_roshi"                    
+            break
+        elif op == "3":
+            catalogo_servicios["tipo_servicios"] = "prepago_Krilin"                    
+            break
+        elif op == "4":
+            catalogo_servicios["tipo_servicios"] = "combo_dragon"                    
+            break
+        else:
+            dm = "Error al registrar tipo_servicios"
+            reportar_error_a_txt(dm)
+            print("El tipo_servicios no es valido")
+            catalogo_servicios["tipo_servicios"] = ""  
+            
+def int_precio(catalogo_servicios):
+    while True:
+        try:
+            catalogo_servicios["precio"] = int(input("Ingrese el precio: "))
+            if isinstance(catalogo_servicios["precio"], int): 
+                catalogo_servicios["precio"] = catalogo_servicios["precio"]
+                break
+            else: raise ValueError("precio ", catalogo_servicios["precio"]," no es valido")
+        except Exception as e:
+            e = e,"Error al registrar precio"
+            reportar_error_a_txt(e)
+            print("Ingrese un precio valido")
+            
+def int_nuevo(catalogo_servicios):
+    while True:
+        try:
+            catalogo_servicios["descuento"]["nuevo"] = int(input("Ingrese el descuento para usuarios nuevos: "))
+            if isinstance(catalogo_servicios["descuento"]["nuevo"], int): 
+                catalogo_servicios["descuento"]["nuevo"] = catalogo_servicios["descuento"]["nuevo"]
+                break
+            else: raise ValueError("El descuento para usuarios nuevos ", catalogo_servicios["descuento"]["nuevo"]," no es valido")
+        except Exception as e:
+            e = e,"Error al registrar descuento para usuarios nuevos"
+            reportar_error_a_txt(e)
+            print("Ingrese un descuento para usuarios nuevos valido")
+            
+def int_regular(catalogo_servicios):
+    while True:
+        try:
+            catalogo_servicios["descuento"]["regular"] = int(input("Ingrese el descuento para usuarios regulares: "))
+            if isinstance(catalogo_servicios["descuento"]["regular"], int): 
+                catalogo_servicios["descuento"]["regular"] = catalogo_servicios["descuento"]["regular"]
+                break
+            else: raise ValueError("El descuento para usuarios regulares ", catalogo_servicios["descuento"]["regular"]," no es valido")
+        except Exception as e:
+            e = e,"Error al registrar descuento para usuarios regulares"
+            reportar_error_a_txt(e)
+            print("Ingrese un descuento para usuarios regulares valido")
+            
+def int_leal(catalogo_servicios):
+    while True:
+        try:
+            catalogo_servicios["descuento"]["leal"] = int(input("Ingrese el descuento para usuarios leales: "))
+            if isinstance(catalogo_servicios["descuento"]["leal"], int): 
+                catalogo_servicios["descuento"]["leal"] = catalogo_servicios["descuento"]["leal"]
+                break
+            else: raise ValueError("El descuento para usuarios leales ", catalogo_servicios["descuento"]["leal"]," no es valido")
+        except Exception as e:
+            e = e,"Error al registrar descuento para usuarios leales"
+            reportar_error_a_txt(e)
+            print("Ingrese un descuento para usuarios leales valido")
